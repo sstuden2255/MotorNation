606c88128f6439aee987147b7a29655080691b82
Vincent <vincentkao2003@gmail.com>
2023-06-02 14:46:30 -0700

    sign up and log in

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json()); // built-in middleware
 // for multipart/form-data (required with FormData)
 app.use(multer().none()); // requires the "multer" module
 
-//
+// for cookies
 app.use(cookieParser());
 
 // gets a list of vehicles matching filter parameters
@@ -43,10 +43,10 @@ app.get("/vehicles", async function(req, res) {
         maxPrice = parseFloat(maxPrice);
       }
       if (type === "all") {
-        query += ";";
+        query += " ORDER BY name;";
         results = await db.all(query, maxPrice);
       } else {
-        query += " AND type LIKE ?;";
+        query += " AND type LIKE ? ORDER BY name;";
         results = await db.all(query, maxPrice, type);
       }
       let cars = "";
@@ -93,17 +93,88 @@ app.get("/reviews/:vehicle_name", async function(req, res) {
 // posts a new review for a vehicle
 app.post("/reviews/new/:vehicle_name", async function(req, res) {
   const name = req.params["vehicle_name"];
-  console.log(name);
   try {
     const login = req.cookies["loggedIn"];
-    await reviewEligibilityCheck();
+    console.log(login);
+    let eligible = true; // await reviewEligibilityCheck(req.cookies);
+    if (req.cookies["loggedIn"] === "false") {
+      res.type("text").status(400).send("Please Log In!");
+    } else {
+
+    }
     res.type("text").send("ok");
   } catch (err) {
-    console.log(err);
+    res.type("text").status(500).send("Something on the server went wrong!");
   }
-
 });
 
+// creates a new user
+app.post("/account/create", async function(req, res) {
+  const username = req.body["username"], email = req.body["email"], password = req.body["password"];
+  try {
+    if (username && email && password) {
+      let db = await getDBConnection();
+      let results = await db.get("SELECT * FROM users WHERE email = ?;", email);
+      if (results) {
+        await db.close();
+        res.type("text").status(400).send("An account with that email address already exists.");
+      } else {
+        let userNameExist = await checkUserName(username);
+        if (userNameExist) {
+          await db.close();
+          res.type("text").status(400).send("Username already taken");
+        } else {
+          let query = "INSERT INTO users (username, email, password, balance) VALUES (?, ?, ?, ?);";
+          results = await db.run(query, [username, email, password, 0]);
+          query = "SELECT username, password FROM users WHERE id = ?;";
+          results = await db.get(query, results["lastID"]);
+          await db.close();
+          res.type("json").send(results);
+        }
+      }
+    } else {
+      res.type("text").status(400).send("A field is missing!");
+    }
+  } catch (err) {
+    res.type("text").status(500).send("Something on the server went wrong!");
+  }
+});
+
+// user log in
+app.post("/login", async function(req, res) {
+  const username = req.body["username"];
+  const password = req.body["password"];
+  try {
+    if (username && password) {
+      let query = "SELECT * FROM users WHERE username = ? AND password = ?;";
+      let db = await getDBConnection();
+      let results = await db.get(query, [username, password]);
+      await db.close();
+      if (results) {
+        const expirationDate = "Fri, 31 Dec 9999 23:59:59 GMT";
+        res.cookie("username", username , { expires: new Date(expirationDate) });
+        res.type("text").send("Logged In Seccussfully");
+      } else {
+        res.type("text").status(400).send("The username/password is incorrect");
+      }
+    } else {
+      res.type("text").status(400).send("A field is missing!");
+    }
+  } catch (err) {
+    res.type("text").status(500).send("Something on the server went wrong!");
+  }
+});
+
+async function checkUserName(username) {
+  try {
+    let query = "SELECT username FROM users WHERE username = ?";
+    let db = await getDBConnection();
+    let results = await db.get(query, username);
+    return results;
+  } catch (err) {
+
+  }
+}
 
 /**
  * Establishes a database connection to the database and returns the database object.
--- tables.sql
+++ tables.sql
@@ -9,20 +9,23 @@ CREATE TABLE "vehicles" (
 );
 
 CREATE TABLE "users" (
+	"id"	INTEGER,
 	"email"	TEXT NOT NULL,
 	"password"	TEXT NOT NULL,
 	"balance"	INTEGER NOT NULL,
-	"first-name"	TEXT NOT NULL,
-	"last-name"	TEXT NOT NULL,
-	PRIMARY KEY("email")
+	"first"	TEXT NOT NULL,
+	"last"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
 CREATE TABLE "reviews" (
+	"id"	INTEGER,
 	"vehicle"	TEXT NOT NULL,
 	"user"	TEXT NOT NULL,
 	"rating"	INTEGER NOT NULL,
 	"comment"	TEXT,
-	"date"	DATETIME DEFAULT (datetime('now', 'localtime'))
+	"date"	DATETIME DEFAULT (datetime('now', 'localtime')),
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
 CREATE TABLE "transactions" (
